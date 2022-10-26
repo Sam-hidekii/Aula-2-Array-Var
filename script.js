9e8f88f6934e40956018e28267b5742633999827
Sam-hidekii <106408406+Sam-hidekii@users.noreply.github.com>
2022-10-26 17:04:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+/*Crie o seguinte array: var deuses = ['Aegir', 'Aud', 'Balder']
+Adicione os 3 Deuses: ('Loki', 'Odin', 'Freya') com a função push()
+
+Crie o seguinte array: var numbers = [5, 7, 1, 8, 9]
+E exiba de forma crescente dica: .sort
+
+Crie o seguinte array: var numbers = [5, 7, 1, 8, 9]
+E exiba de forma decrescente.*/
+
+var deuses = ['aegir','aud','balder'];
+deuses.push('loki','odin','freya');
+console.log(deuses);
+
+ var number = [ 5, 7, 1, 8, 9];
+
+ number.sort();
+ console.log(number);
+
+ number.reverse();
+ console.log(number);
+
+
